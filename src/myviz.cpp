821ac40b17051fd5c767e3badad02700f3a195b8
nikisalli <niki.salli@gmail.com>
2021-03-27 00:08:07 +0100

    added tf

--- src/myviz.cpp
+++ src/myviz.cpp
@@ -10,31 +10,42 @@
 #include "myviz.h"
 
 MyViz::MyViz( QWidget* parent ) : QWidget( parent ){
-  render_panel_ = new rviz::RenderPanel();
-  QVBoxLayout* main_layout = new QVBoxLayout;
-  main_layout->addWidget( render_panel_ );
+    render_panel_ = new rviz::RenderPanel();
+    QVBoxLayout* main_layout = new QVBoxLayout;
+    main_layout->addWidget( render_panel_ );
 
-  setLayout( main_layout );
+    setLayout( main_layout );
 
-  manager_ = new rviz::VisualizationManager( render_panel_ );
-  render_panel_->initialize( manager_->getSceneManager(), manager_ );
-  manager_->initialize();
-  manager_->startUpdate();
+    manager_ = new rviz::VisualizationManager( render_panel_ );
+    render_panel_->initialize( manager_->getSceneManager(), manager_ );
+    manager_->initialize();
+    manager_->startUpdate();
 
-  // Create rviz components
-  grid_ = manager_->createDisplay( "rviz/Grid", "adjustable grid", true );
-  ROS_ASSERT( grid_ != NULL );
-  robot_model_ = manager_->createDisplay( "rviz/RobotModel", "adjustable grid", true );
-  ROS_ASSERT( robot_model_ != NULL );
+    // Create rviz components
+    grid_ = manager_->createDisplay( "rviz/Grid", "adjustable grid", true );
+    ROS_ASSERT( grid_ != NULL );
+    robot_model_ = manager_->createDisplay( "rviz/RobotModel", "robot model", true );
+    ROS_ASSERT( robot_model_ != NULL );
+    tf_ = manager_->createDisplay( "rviz/TF", "tf", true );
+    ROS_ASSERT( tf_ != NULL );
 
-  // Configure Grid
-  grid_->subProp( "Line Style" )->setValue( "Billboards" );
-  grid_->subProp( "Color" )->setValue( QColor(Qt::white) );
-  grid_->subProp( "Line Style" )->subProp( "Line Width" )->setValue(0.01);
-  grid_->subProp( "Cell Size" )->setValue(1);
+    // Configure rviz
+    manager_->setFixedFrame( "map" );
 
-  // Configure RobotModel
-  robot_model_->subProp( "Robot Description" )->setValue( "robot_description" );
+    // Configure Grid
+    grid_->subProp( "Line Style" )->setValue( "Billboards" );
+    grid_->subProp( "Color" )->setValue( QColor(Qt::white) );
+    grid_->subProp( "Line Style" )->subProp( "Line Width" )->setValue(0.01);
+    grid_->subProp( "Cell Size" )->setValue(1);
+
+    // Configure RobotModel
+    robot_model_->subProp( "Robot Description" )->setValue( "robot_description" );
+
+    // Configure TF
+    tf_->subProp( "Show Names" )->setValue(false);
+    tf_->subProp( "Show Axes" )->setValue(true);
+    tf_->subProp( "Show Arrows" )->setValue(true);
+    tf_->subProp( "Marker Scale" )->setValue(0.3);
 }
 
 // Destructor.
--- src/myviz.h
+++ src/myviz.h
@@ -3,27 +3,24 @@
 
 #include <QWidget>
 
-namespace rviz
-{
-class Display;
-class RenderPanel;
-class VisualizationManager;
+namespace rviz{
+    class Display;
+    class RenderPanel;
+    class VisualizationManager;
 }
 
-// BEGIN_TUTORIAL
-// Class "MyViz" implements the top level widget for this example.
-class MyViz: public QWidget
-{
-Q_OBJECT
-public:
-  MyViz( QWidget* parent = 0 );
-  virtual ~MyViz();
+class MyViz: public QWidget{
+    Q_OBJECT
+    public:
+        MyViz( QWidget* parent = 0 );
+        virtual ~MyViz();
 
-private:
-  rviz::VisualizationManager* manager_;
-  rviz::RenderPanel* render_panel_;
-  rviz::Display* grid_;
-  rviz::Display* robot_model_;
+    private:
+        rviz::VisualizationManager* manager_;
+        rviz::RenderPanel* render_panel_;
+        rviz::Display* grid_;
+        rviz::Display* robot_model_;
+        rviz::Display* tf_;
 };
-// END_TUTORIAL
+
 #endif // MYVIZ_H
